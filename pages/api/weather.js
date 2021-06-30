49faeb7e514ded842bbf11dd8c6498706ecbb008
Ahmed <elazzabi.ahmed@gmail.com>
2021-06-30 15:54:11 +0100

    rename _ to req

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -1,7 +1,7 @@
 import weather from '@/lib/weather';
 
-export default async function handler( _, res ) {
-	return weather( _.query.city ).then( r => {
+export default async function handler( req, res ) {
+	return weather( req.query.city ).then( r => {
 		return res.status( 200 ).send( r );
 	} ).catch( e => {
 		return res.status( 500 ).send( e );

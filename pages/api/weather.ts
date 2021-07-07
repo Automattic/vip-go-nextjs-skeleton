6ccf47770bbec7d6e3da331c335e5516f35a2807
Ahmed <elazzabi.ahmed@gmail.com>
2021-07-07 11:18:45 +0100

    Add redis implementation to the skeleton (#6)
    * add ioredis
    
    * add weather api implementation
    
    * rename _ to req
    
    * add expiration of 10 seconds
    
    * only set in redis if client available
    
    * transform api page to .tsx
    
    * import fetch from 'node-fetch'
    
    * rename weather file from .tsx => .ts

---
+++ pages/api/weather.ts
@@ -0,0 +1,10 @@
+import weather from '@/lib/weather';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler( req: NextApiRequest, res: NextApiResponse ) {
+	return weather( req.query.city ).then( r => {
+		return res.status( 200 ).send( r );
+	} ).catch( e => {
+		return res.status( 500 ).send( e );
+	} )
+}

8d868ca32f37f50f75e299a81edba5f3a2d0872c
chriszarate <chris.zarate@automattic.com>
2021-07-09 08:59:16 -0600

    Type fixes

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -2,7 +2,7 @@ import weather from '@/lib/weather';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse ) {
-	return weather( req.query.city ).then( r => {
+	return weather( `${req.query.city}` ).then( r => {
 		return res.status( 200 ).send( r );
 	} ).catch( e => {
 		return res.status( 500 ).send( e );

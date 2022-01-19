22a6723a1b8c9a134e14e18d58837661de2b75f3
chriszarate <chris.zarate@automattic.com>
2022-01-19 15:20:13 -0500

    Add support for innerBlocks

--- components/UnsupportedBlock/UnsupportedBlock.tsx
+++ components/UnsupportedBlock/UnsupportedBlock.tsx
@@ -9,6 +9,7 @@ export default function UnsupportedBlock ( props: Props ) {
 	return (
 		<div className={styles.container}>
 			<h4 className={styles.title}><strong>Unsupported block</strong>: <code>{props.block.name}</code></h4>
+			<h5>{props.block.tagName}</h5>
 			<blockquote>
 				{props.block.innerHTML}
 			</blockquote>
@@ -18,6 +19,17 @@ export default function UnsupportedBlock ( props: Props ) {
 						{
 							props.block.attributes.map( ( attr, i ) => (
 								<li key={i}><strong>{attr.name}</strong>: {attr.value}</li>
+							) )
+						}
+					</ul>
+				)
+			}
+			{
+				props.block.innerBlocks.length > 0 && (
+					<ul>
+						{
+							props.block.innerBlocks.map( ( block, i ) => (
+								<li key={i}><strong>{block.name}</strong>: {block.tagName}</li>
 							) )
 						}
 					</ul>

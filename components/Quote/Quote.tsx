74f0d51a64f1398a5f654bb81d9ba271c4765ea1
Roger Theriault <roger.theriault@automattic.com>
2022-01-21 16:17:19 -0500

    style the two Quote options, and add commentary

--- components/Quote/Quote.tsx
+++ components/Quote/Quote.tsx
@@ -1,5 +1,26 @@
 import styles from './Quote.module.css';
 
+/**
+ * This is a styled component.
+ *
+ * The WordPress block will have content (passed as innerHTML):
+ *      <p>Before software can be reusable, it first has to be usable.</p><cite>Ralph Johnson</cite>
+ * And a className attribute that is either is-style-default or is-style-large
+ *
+ * The PostContent.tsx will set the large prop to the className and deliver the innerHTML as is.
+ *
+ * The Quote component is output as:
+ *      <blockquote>
+ *          <p>Before software can be reusable, it first has to be usable.</p><cite>Ralph Johnson</cite>
+ *      </blockquote>
+ *
+ * The className varies, and refers to the imported styles in Quote.module.css
+ *
+ * Quote.module.css contains two different styling options - large or default.
+ * The large option results in a defined separation vertically between quote and citation, while the default
+ * is much more compact.
+ */
+
 type Props = {
 	innerHTML: string,
     large?: string,

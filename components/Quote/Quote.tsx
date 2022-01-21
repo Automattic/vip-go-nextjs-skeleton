88ccf3638df638f00128afc24dd20a830670b34e
Roger Theriault <roger.theriault@automattic.com>
2022-01-21 16:34:46 -0500

    fix some spacing

--- components/Quote/Quote.tsx
+++ components/Quote/Quote.tsx
@@ -23,13 +23,13 @@ import styles from './Quote.module.css';
 
 type Props = {
 	innerHTML: string,
-    large?: string,
+	large?: string,
 };
 
 export default function Quote ( props: Props ) {
 	return (
 		<blockquote
-			className={props.large == 'is-style-large' ? styles.largecontainer : styles.container}
+			className={ props.large == 'is-style-large' ? styles.largecontainer : styles.container }
 			dangerouslySetInnerHTML={ { __html: props.innerHTML } }
 		/>
 	);

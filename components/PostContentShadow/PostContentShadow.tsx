4ef68460dd20ab7c7200aa1a4d25fa6aa3d4651e
Alec Geatches <alec.geatches@automattic.com>
2022-12-30 15:06:05 +0800

    Add react-shadow example

--- components/PostContentShadow/PostContentShadow.tsx
+++ components/PostContentShadow/PostContentShadow.tsx
@@ -1,19 +1,17 @@
-import { useEffect } from 'react';
+import root from 'react-shadow';
 
 type Props = {
 	content: string,
 };
 
 export default function PostContentShadow( { content } : Props ) {
-	useEffect( () => {
-		import("../WPContentWrapper/WPContentWrapper");
-	}, [] );
+	return (
+		<root.div>
+			<link rel="stylesheet" href="http://decoupled.vipdev.lndo.site/wp-includes/css/dist/block-library/style.css"/>
+			<link rel="stylesheet" href="http://decoupled.vipdev.lndo.site/wp-includes/css/dist/block-library/theme.css"/>
+			<link rel="stylesheet" href="http://decoupled.vipdev.lndo.site/wp-json/vip-decoupled-styles/v1/styles"/>
 
-	return <>
-		<code style={{ "margin": "0 0 2rem 0", "display": "block" }}>Shadow content:</code>
-
-		<wp-content-wrapper data-content={content}></wp-content-wrapper>
-
-		<code style={{ "margin": "2rem 0 0 0", "display": "block" }}>End of shadow content</code>
-	</>;
+			<body dangerouslySetInnerHTML={{ __html: content }} />
+		</root.div>
+	);
 }

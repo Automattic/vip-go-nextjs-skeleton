311eb62c30667e8d3df68ac372c6075022787162
ostowe <2472666+ostowe@users.noreply.github.com>
2023-03-13 13:42:14 -0400

    Remove x-middleware-prefetch header from all responses

--- middleware.ts
+++ middleware.ts
@@ -5,11 +5,23 @@ import { NextMiddleware, NextRequest, NextResponse } from 'next/server';
 // https://nextjs.org/docs/middleware
 
 export const middleware: NextMiddleware = ( req: NextRequest ) => {
+	// Remove x-middleware-prefetch header to prevent VIP's infrastructure
+	// from caching empty JSON responses on prefetched data for SSR pages. See the following URLs for more info:
+	// https://github.com/vercel/next.js/discussions/45997
+	// https://github.com/vercel/next.js/pull/45772
+	// https://github.com/vercel/next.js/blob/v13.1.1/packages/next/server/base-server.ts#L1069
+	const headers = new Headers(req.headers);
+  headers.delete('x-middleware-prefetch');
+
 	// Required health check endpoint on VIP. Do not remove.
 	if ( req.nextUrl.pathname === '/cache-healthcheck' ) {
 		return NextResponse.rewrite( new URL( '/api/healthcheck', req.url ) );
 	}
 
 	// Continue as normal through the Next.js lifecycle.
-	return NextResponse.next();
+	return NextResponse.next({
+    request: {
+      headers,
+    },
+  });
 }
